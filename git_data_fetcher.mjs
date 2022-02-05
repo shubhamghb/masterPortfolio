260741c6eebd8e1544c1b37da24c8e83e2b78485
Shubham Maheshwari <77975234+shubhamghb@users.noreply.github.com>
2022-02-05 05:34:20 +0530

    init

--- git_data_fetcher.mjs
+++ git_data_fetcher.mjs
@@ -2,8 +2,8 @@ import fetch from 'node-fetch';
 import fs from 'fs';
 
 const openSource = {
-  githubConvertedToken: "Your Github Token Here",
-  githubUserName: "Your Github Username Here",
+  githubConvertedToken: "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",
+  githubUserName: "shubhamghb",
 };
 
 const query_pr = {
